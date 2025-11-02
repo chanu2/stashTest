423caadb02399b515c15c553f0bad8ec22d90a72
canu <mds558@dayside.co.kr>
2025-11-02 18:26:53 +0900

    tet

---
+++ demo/demo/src/main/java/com/example/demo/TestClass.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class TestClass {
+
+    public static void main(String[] args) {
+        int a1 = 10;
+        int a2 = 10;
+        int a3 = 10;
+        int a4 = 10;
+        int a5 = 10;
+        int a6 = 10;
+        int a7 = 10;
+        int a8 = 10;
+        int a9 = 10;
+        int a10 = 10;
+    }
+}

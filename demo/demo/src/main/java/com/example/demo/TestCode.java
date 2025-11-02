07c9c1197e65765337c1abc4c3be4b0fbbd8fd01
canu <mds558@dayside.co.kr>
2025-11-02 18:32:53 +0900

    tet

---
+++ demo/demo/src/main/java/com/example/demo/TestCode.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class TestCode {
+
+    public static void main(String[] args) {
+
+
+        int test1 = 10;
+        int test2 = 10;
+        int test3 = 10;
+        int test4 = 10;
+    }
+}
